abfe5695b4083954feb7831774fddb4325b8e634
Matthew Markic <matt.markic@hotmail.com>
2016-10-10 22:46:11 +0000

    This program downloads the avatar images for all the contributors on a specific github repo

---
+++ avatar-downloader/download_avatars.js
@@ -0,0 +1,28 @@
+'use strict';
+const request = require('request');
+const fs = require('fs');
+
+const owner = process.argv[2];
+const repo = process.argv[3];
+
+const getRepoContributors = function(repoOwner, repoName, cb) {
+  const root = 'https://api.github.com';
+  const options = {
+    url:`${root}/repos/${repoOwner}/${repoName}/contributors`,
+    headers: {
+      'User-Agent': 'request'
+    }
+  }
+  request.get(options, cb);
+};
+
+const downloadImageByURL = function(url, filePath) {
+  request(url).pipe(fs.createWriteStream(filePath));
+}
+
+getRepoContributors(owner, repo, function (err, res, body) {
+  const contributors = JSON.parse(body);
+    contributors.forEach(function(elm){
+      downloadImageByURL(elm.avatar_url, `./avatars/${elm.login}.png`);
+    })
+})
