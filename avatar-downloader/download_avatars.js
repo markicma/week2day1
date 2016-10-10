431e66768f559ccf7e86fdd9d5afe4d57c430c24
Matthew Markic <matt.markic@hotmail.com>
2016-10-10 23:03:47 +0000

    Added comments to descibe what's happening in the code

--- avatar-downloader/download_avatars.js
+++ avatar-downloader/download_avatars.js
@@ -1,10 +1,16 @@
 'use strict';
+// Importing the functionality of the request and fs packages
 const request = require('request');
 const fs = require('fs');
 
+// Including the command line functionality of the third and fourth parameters
 const owner = process.argv[2];
 const repo = process.argv[3];
 
+// The getRepoContributors function has two parameters, the repo owner name and
+// the name of the repo. The endpoint using those parameters is then added to the
+// root. From this link, the function gets the JSON file in a string format and
+// the callback function is applied to it.
 const getRepoContributors = function(repoOwner, repoName, cb) {
   const root = 'https://api.github.com';
   const options = {
@@ -16,6 +22,11 @@ const getRepoContributors = function(repoOwner, repoName, cb) {
   request.get(options, cb);
 };
 
+// The downloadImageByURL takes two parameters, the image url and the download
+// location. The function checks to see if the avatars folder exists in
+// the current folder and if not present, creates one.
+// Then using the request and fs packages downloads the images into the specified
+// foler.
 const downloadImageByURL = function(url, filePath) {
   let dir = './avatars'
   if (!fs.existsSync(dir)) {
@@ -24,6 +35,10 @@ const downloadImageByURL = function(url, filePath) {
   request(url).pipe(fs.createWriteStream(filePath));
 }
 
+// Calls the getRepoContributors with the third and fourth parameter from the
+// command line. The JSON file in string format is parsed into an array of objects.
+// In each object in the array the avatar url is selected and the image is downloaded
+// into the avatars folder using the username of the contributor. 
 getRepoContributors(owner, repo, function (err, res, body) {
   const contributors = JSON.parse(body);
     contributors.forEach(function(elm){
